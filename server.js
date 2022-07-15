6d72c5938c1e1d37333c4730515629c717416a5a
d0iloppa <kdi3939@gmail.com>
2022-07-15 13:51:23 +0900

    VAPID KEY 초기생성 및 설정 저장 동기화 작업

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ var webpush = require("web-push"); // web-push 사용
 // const d_Conf = require("./conf");
 const conf_dir = __dirname + "/";
 const conf_file = "conf.json";
-// 
+
+// 기본 conf파일 템플릿
 const tmpConf = {
   "domain" : "localhost",
   "port" : 80,
@@ -25,21 +26,38 @@ const tmpConf = {
 
 
 // @ 설정파일 체크
-var jsonConf;
+var d_Conf;
 var vapidKeys;
 
 // vapid값이 설정되어 있지 않으면 생성하고, 정해져있으면 설정
 function ifNot_gen_setVAPID(){
   if(fs.existsSync(conf_dir + conf_file)){
 
-    jsonConf = JSON.parse( fs.readFileSync(conf_dir + conf_file , 'utf8') );
+    d_Conf = JSON.parse( fs.readFileSync(conf_dir + conf_file , 'utf8') );
   
-    if(jsonConf.VAPID){
-      vapidKeys = jsonConf.VAPID
+    if(d_Conf.VAPID){
+      vapidKeys = d_Conf.VAPID
     } else{
       const tmp = webpush.generateVAPIDKeys();
-      jsonConf.VAPID = tmp;
+      d_Conf.VAPID = tmp;
+      vapidKeys = tmp;
+
+      setVapidToConf(vapidKeys);
     }
+  }
+
+}
+
+
+function setVapidToConf(vapid_key){
+  if(fs.existsSync(conf_dir + conf_file)) {
+    let tmp = JSON.parse( fs.readFileSync(conf_dir + conf_file , 'utf8') );
+    tmp.VAPID = vapid_key;
+
+    console.log("set VAPID to config file [info]: " , tmp);
+
+
+    fs.writeFileSync(conf_dir + conf_file , JSON.stringify(tmp) );
   }
 }
 
@@ -47,32 +65,13 @@ if(fs.existsSync(conf_dir + conf_file)){
   // @ 확장자 체크 (json 파일이 아닌 경우 예외처리)
   const i = conf_file.lastIndexOf('.');
   var ext = (i < 0) ? false : conf_file.substring(i + 1);
-  if(ext == 'json'){
-    // conf파일에 VAPID KEY값이 있는지 확인
-    ifNot_gen_setVAPID();
-
-    /*
-    jsonConf = JSON.parse( fs.readFileSync(conf_dir + conf_file , 'utf8') );
-    console.log(jsonConf);
-
-    if(jsonConf.VAPID){
-      vapidKeys = jsonConf.VAPID
-    } else{
-      const tmp = webpush.generateVAPIDKeys();
-      jsonConf.VAPID = tmp;
-
-      console.log(jsonConf);
-    }
-    */
-  }
-
-
-
+  if(ext == 'json') ifNot_gen_setVAPID();
   
 }else{
   fs.writeFileSync(conf_dir + conf_file , JSON.stringify(tmpConf) , (err) => console.log(err));
-  jsonConf = JSON.parse( fs.readFileSync(conf_dir + conf_file , 'utf8') );
+  d_Conf = JSON.parse( fs.readFileSync(conf_dir + conf_file , 'utf8') );
   console.log("conf파일 생성완료");
+  ifNot_gen_setVAPID();
 }
 
 
@@ -103,7 +102,7 @@ const credentials = {
 
 // vapid key 생성 및 push lib에 연동
 
-const { json } = require("express");
+// const { json } = require("express");
 //const vapidKeys = d_Conf.VAPID ? d_Conf.VAPID : webpush.generateVAPIDKeys();
 
 
@@ -142,8 +141,7 @@ doilServer.get("/getClientIp.do", (req, res) => {
 
 
 /*******************[구독 관련 annotation]****************************/
-// 구독자 array
-// const temp_subs = [];
+// 구독자 array (SERVER IN-MEMORY)
 var subs_Arr = [];
 
 
@@ -154,42 +152,9 @@ doilServer.get("/getServerKey.do", (req, res) => {
   res.json(sendData);
 });
 
+/*
 doilServer.post("/getService-worker.do", (req, res) => {
   res.sendFile(__dirname + "/public/js/client.js");
-});
-
-/*
-// 구독자 리스트 getter
-doilServer.get("/getSubArr.do", (req, res) => {
-  console.log(temp_subs);
-  res.send(temp_subs);
-});
-*/
-/*
-// 구독자 전체에게 push
-doilServer.get("/push.do", (req, res) => {
-  console.log("push start");
-  var idx = 0;
-  const term = 1000;
-  console.log("[sub size] : " + temp_subs.length);
-  for (const sub of temp_subs) {
-    // 구독자 정보가 올바른 경우에만 작동
-    const isCorrectTemplate = sub["endpoint"] ? true : false;
-    if (isCorrectTemplate) {
-      console.log(`Sub_info #${idx++} :  ${JSON.stringify(sub)}`);
-      webpush
-        .sendNotification(sub, "test message")
-        .then((res) => {
-          console.log("sent notification successfully");
-        })
-        .catch((err) => {
-          console.error(`notification error : ${err}`);
-          res.sendStatus(500);
-        });
-    } else continue;
-  }
-  console.log("push end");
-  //res.send("Push complete");
 });
 */
 
@@ -241,16 +206,6 @@ doilServer.get("/test-push.do", multipartMiddleware, (req, res) => {
         "auth":"Bjna681paxHEXFNWcwbcRg"
       }
     };
-
-  /*{
-    "endpoint":"https://wns2-pn1p.notify.windows.com/w/?token=BQYAAAAHoGsKgvDWJJ9qAL1PeLZ7TnikAXTECfUbyPk4VUMQhfvH6NbLFf5U5jH0h5kqSeivQZ3PpPZ8FRURnJL2UION4E8YvZ%2bfJeqkHi%2bLZ611Xi6Rq6QMEzBTlscrPPz22eGs6qRU%2fXf3U1f%2fh3YC1hyd7qbox5TPczVeBuZRwNdZFTletNbZgWtrL8Sgcrv7NGld9XLK9J0y1cpudgnZqg%2bmBk2LprB%2b51QM16qVf%2bAMynLcTsBeNGDJgTFz4mB1GZ%2b4a7w5izB3d9Siw05lR5DZcORqkg%2fKP5mwaRkprGwYQ7t3deOzavDazTV8lwB7BVGYSTwUWfaeSyQINXgqZdkW",
-    "expirationTime":null,
-    "keys":{
-      "p256dh":"BIRMMTEY6-uOUGYX-XBh7mJA7DPS8LntiCOCSPSzA-YudTEMJWVZCUglyho21CTonrtFfhV_dw33iX4L1sGDro4",
-      "auth":"qk3XW27x5f7gjcX1flV7SQ"
-    }
-  };
-  */
 
 
 
@@ -313,7 +268,7 @@ doilServer.post("/browser-close.do", multipartMiddleware, (req, res) => {
 
 /****************************************************************/
 
-/*
+
 const httpServer = doilServer.listen(d_Conf["port"], () => {
   console.log(`
     ##########################################################
@@ -335,7 +290,6 @@ const httpsServer = https
     `);
   });
 
-  */
 
 /*
 var io = require("socket.io")(httpsServer);
@@ -343,7 +297,7 @@ io.on("connection", (socket) => {
   //연결이 들어오면 실행되는 이벤트
   // socket 변수에는 실행 시점에 연결한 상대와 연결된 소켓의 객체가 들어있다.
   console.log(`
-    =======================================${socket.id}connected...
+    ${socket.id} connected... ==============
   `);
 
 
@@ -351,7 +305,7 @@ io.on("connection", (socket) => {
   // user connection lost
   socket.on("disconnect", function (data) {
     console.log(`
-      ==============================================${socket.id} user disconnected====================================
+      ${socket.id} user disconnected ==============
     `);
   });
 });
